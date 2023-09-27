13967f1db4cab056d8f63988a2b1ae75dd7a81eb
malin <mal3eine@hotmail.com>
2023-09-27 17:42:18 +0200

    Added WeatherControl

--- ServiceApplication/MVVM/Controls/DateTimeControl.xaml.cs
+++ ServiceApplication/MVVM/Controls/DateTimeControl.xaml.cs
@@ -1,28 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+using System.ComponentModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ServiceApplication.MVVM.Controls
 {
-    /// <summary>
-    /// Interaction logic for DateTimeControl.xaml
-    /// </summary>
-    public partial class DateTimeControl : UserControl
+
+    // INotifyPropertyChanged är ett interface som lyssnar (i bakgrunden) på förändringar.
+    // Detta gör att frontend kan ändras, att det grafiska gränssnittet kan ändras utan
+    // att låsa upp tråden. 
+    public partial class DateTimeControl : UserControl, INotifyPropertyChanged
     {
+
+        // De publika properties måste mellanlagras i privata variabler. 
+        private string? _currentTime;
+        private string? _currentDate;
+
+        public string? CurrentTime
+        {
+            get => _currentTime;
+            set { _currentTime = value; OnPropertyChanged(nameof(CurrentTime)); }
+        }
+        public string? CurrentDate
+        {
+            get => _currentDate;
+            set { _currentDate = value; OnPropertyChanged(nameof(CurrentDate)); }
+        }
+
+
+        // Denna implementeras från interfacet. 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        // Metod som lyssnar på förändringar.
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        // Konstruktor
         public DateTimeControl()
         {
             InitializeComponent();
+            DataContext = this;
+
+            // Här kör vi metoden som set:ar tid och datum så kommer vi att få tid och datum i vyn. 
+            SetDateTime();
+
+            // Lägg till ett namespace.
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                // Här säger vi att klockan ska uppdateras varje sekund. 
+                Interval = TimeSpan.FromSeconds(1),
+            };
+            // Här set:ar vi tid och datum
+            timer.Tick += (s, e) => SetDateTime();
+            timer.Start();
+        }
+
+
+        // Metod för att sätta datum och tid. 
+        private void SetDateTime()
+        {
+            CurrentTime = DateTime.Now.ToString("HH:mm");
+            // dddd: Dagens namn, d: Datumet, MMMM: Hela månaden, yyyy: Hela årtalet
+            CurrentDate = DateTime.Now.ToString("dddd, d MMMM yyyy");
         }
     }
 }
---
+++ ServiceApplication/MVVM/Controls/WeatherControl.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace ServiceApplication.MVVM.Controls
+{
+    public partial class WeatherControl : UserControl, INotifyPropertyChanged
+    {
+
+        private string? _temperature;
+        private string? _condition;
+        // Mappa om till privat variabel 
+        public string? Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; OnPropertyChanged(nameof(Temperature)); }
+        }
+        public string? Condition
+        {
+            get { return _condition; }
+            set { _condition = value; OnPropertyChanged(nameof(Condition)); }
+        }
+
+
+
+        // Implementerar interfacet INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+
+        // Konstruktor
+        public WeatherControl()
+        {
+            InitializeComponent();
+            DataContext = this; //mappar frontend-backend
+
+
+            // En timer som uppdaterar vädret var 15:e minut.
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMinutes(15)
+            };
+            // Metoden körs async. 
+            timer.Tick += async (s, e) => await GetWeatherAsync();
+            // Kör denna metod också. När timern laddas ska den köra
+            // den här det första den gör.
+            Loaded += async (s, e) => await GetWeatherAsync();
+            // Starta timer.
+            timer.Start();
+        }
+
+
+        // Hämta väder från ett api.
+        private async Task GetWeatherAsync()
+        {
+            using HttpClient http = new HttpClient();
+            try
+            {
+                var data = JsonConvert.DeserializeObject<dynamic>(await http.GetStringAsync(
+                    "https://api.openweathermap.org/data/2.5/weather?lat=59.1875&lon=18.1232&appid=b4a3119e986341f8f3a4d159c5787679"));
+                Temperature = (data!.main.temp - 273.15).ToString("#,##0.0"); //konverterar till fahrenheit
+
+                // Här ska vi även hämta conditiondelen.
+                switch (data!.weather[0].description.ToString())
+                {
+                    case "clear sky":
+                        Condition = "\ue28f";
+                        break;
+                    case "few clouds":
+                        Condition = "\uf6c4";
+                        break;
+                    case "overcast clouds":
+                        Condition = "\uf744";
+                        break;
+                    case "scattered clouds":
+                        Condition = "\uf0c2";
+                        break;
+                    case "broken clouds":
+                        Condition = "\uf744";
+                        break;
+                    case "shower rain":
+                        Condition = "\uf738";
+                        break;
+                    case "rain":
+                        Condition = "\uf740";
+                        break;
+                    case "thunderstorm":
+                        Condition = "\uf76c";
+                        break;
+                    case "snow":
+                        Condition = "\uf742";
+                        break;
+                    case "mist":
+                        Condition = "\uf74e";
+                        break;
+
+                    default:
+                        Condition = "\ue137";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to get weatherdata. Error: {ex.Message}");
+                Temperature = "--";
+            }
+        }
+    }
+}
