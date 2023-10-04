4d85abfc2c30dd57e5ca678d24e7fa2402b46467
malin <mal3eine@hotmail.com>
2023-10-04 16:06:26 +0200

    Added connection CosmosDb

---
+++ FunctionCosmosDbConnection/Models/DataMessage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionCosmosDbConnection.Models
+{
+    public class DataMessage
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }       
+        public PartitionKey PartitionKey { get; set; } = 
+            new PartitionKey("TemperatureMessage"); 
+        // Guid.NewGuid().ToString());
+    }
+}
