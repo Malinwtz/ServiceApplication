44efa2eb46b622f6025161aea07c40ed5d5c4557
malin <mal3eine@hotmail.com>
2023-10-11 15:51:39 +0200

    Added new iothubmanager in azure function

--- ConnectDeviceToHubFunction/Program.cs
+++ ConnectDeviceToHubFunction/Program.cs
@@ -1,9 +1,13 @@
+using DataAccess.Contexts;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SharedLibrary.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services=>
     {
+        services.AddDbContext<ApplicationDbContext>();
         services.AddSingleton<IotHubManager>();
     })
     .Build();
